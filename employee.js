9d7398abffed0412f530b77e210ab32b00e2f0ff
JBasias <jbasias@gmail.com>
2023-12-12 18:38:40 -0500

    a needed commit

--- employee.js
+++ employee.js
@@ -1,1 +1,23 @@
 // Your code here
+
+class Employee{
+
+    consturctor(name ,  occupation)
+    {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    sayName()
+    {
+        console.log(`${this.name} says hello`);
+    }
+
+    sayOccupation()
+    {
+       console.log(`Employee's name and occupation like so: ${this.name} is a ${this.occupation}`);
+    }
+
+}
+
+module.exports = Employee;
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
+const Employee = require('./employee');
+
+//Employee = require('./employee');
+
+john = new Employee("John Wick" ,  "Dog Lover");
+
+setTimeout(john.sayName, "500");
+
+setTimeout(john.sayOccupation, "500");
+
+
 // Your code here
